ac4182e2fde82c4d2d92d37260ec6cbeb71964b5
EduardovEduard <troyan666-92@mail.ru>
2015-10-16 23:38:42 +0300

    Session management

--- App/App.py
+++ App/App.py
@@ -2,17 +2,22 @@ __author__ = 'eduar'
 
 from flask import Flask
 from collections import defaultdict
+
 app = Flask(__name__)
 
 
-context = defaultdict(dict)
+context = defaultdict(lambda: defaultdict(dict))
 
 
 @app.route('/<string:login>/', defaults={'path': ''})
 @app.route('/<string:login>/<path:path>')
 def request(login, path):
-    print(login, path)
-    return path
+    if 'status' not in context[login]:
+        context[login]['status'] = 0
+    context[login]['status'] += 1
+
+    print(context[login])
+    return 'Your request is number: {}'.format(context[login]['status'])
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
