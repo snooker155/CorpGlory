a545aee4c6356a031db3a0aabec5eac075cad6dc
Константин <bakharev.k@gmail.com>
2015-10-17 01:43:11 +0300

    Graph users generation

--- Models/User.py
+++ Models/User.py
@@ -1,1 +1,36 @@
 __author__ = 'eduar'
+
+import numpy as np
+import random
+
+class User():
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+        self.friends = []
+
+    def update_env(self, env):
+        pass
+
+    def __str__(self):
+        return self.name + "\n" + " ".join([friend.name for friend in self.friends])
+
+class UsersGenerator():
+    @staticmethod
+    def generate_users(num, k=10):
+        def gen_name():
+            return "".join([chr(random.randint(ord('a'), ord('z'))) for _ in range(5)])
+
+        users = [User(id, gen_name()) for id in range(num)]
+        return UsersGenerator.generate_friendships(users, k)
+
+    @staticmethod
+    def generate_friendships(users, k):
+        followers_size = np.random.poisson(k, len(users))
+        for i, user in enumerate(users):
+            followers_indexes = np.random.permutation(len(users))[:followers_size[i]]
+            user.friends = [users[i] for i in followers_indexes]
+        return users
+
+# users = UsersGenerator.generate_users(100)
+# print(users[5])
