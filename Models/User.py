cf617068a05448b6e1ecc5ed617854f1f8f5775c
Константин <bakharev.k@gmail.com>
2015-10-17 03:13:14 +0300

    Simple evolution  of loyalty

--- Models/User.py
+++ Models/User.py
@@ -1,5 +1,3 @@
-__author__ = 'eduar'
-
 import numpy as np
 import random
 
@@ -7,28 +5,68 @@ from collections import namedtuple
 
 Follower = namedtuple('Follower', ['user', 'weight'])
 
-class User():
-    def __init__(self, id, name):
+
+class UsersParameters:
+    def __init__(self, loyalty):
+        self.loyalty = loyalty
+
+
+class User:
+    def __init__(self, id, name, loyalty):
         self.id = id
         self.name = name
         self.friends = []
+        self.parameters = UsersParameters(loyalty)
+        self.old_parameters = UsersParameters(loyalty)
 
-    def update_env(self, env):
+    def update_world(self, world):
         pass
 
+    def update_properties(self):
+        self.old_parameters = self.parameters
+
+    @property
+    def loyalty(self):
+        return self.old_parameters.loyalty
+
     def __str__(self):
-        return self.name + "\n" + " ".join([friend.user.name for friend in self.friends])
+        return str(self.loyalty)
+        # res = "{} {}\n".format(self.name, self.loyalty)
+        # for friend in self.friends:
+        #     res += str(friend.weight) + " " + str(friend.user.loyalty) + ", "
+        # return res
 
 
-class UsersGenerator():
-    WEIGHT_MAX = 10
+    # TODO bakharevk: a * self + (1 - a) * other
+    def recalc(self):
+        def change(w):
+            if self.id == 0:
+                self.parameters.loyalty = 1
+            else:
+                self.parameters.loyalty = (self.old_parameters.loyalty + w) * 0.5
+
+        w = 0
+        for friend in self.friends:
+            w += friend.weight * friend.user.loyalty
+        w /= len(self.friends)
+
+        change(w)
+
+
+class UsersGenerator:
+    WEIGHT_MAX = 1
+    LOYALTY_MAX = 2
 
     @staticmethod
     def generate_users(num, k=10):
-        def gen_name():
+        def name():
             return "".join([chr(random.randint(ord('a'), ord('z'))) for _ in range(5)])
 
-        users = [User(id, gen_name()) for id in range(num)]
+        def loyalty(id):
+            return 0 if id > 0 else 1
+            # return random.random() * UsersGenerator.LOYALTY_MAX - 1.0
+
+        users = [User(id, name(), loyalty(id)) for id in range(num)]
         return UsersGenerator.generate_friendships(users, k)
 
     @staticmethod
@@ -39,5 +77,17 @@ class UsersGenerator():
             user.friends = [Follower(users[i], random.random() * UsersGenerator.WEIGHT_MAX) for i in followers_indexes]
         return users
 
-# users = UsersGenerator.generate_users(100)
-# print(users[5].friends)
+
+users = UsersGenerator.generate_users(100)
+
+for i in range(100):
+    print(str(i) + " : " + str(sum(user.loyalty for user in users)))
+
+    for user in users:
+        user.recalc()
+
+    for user in users:
+        user.update_properties()
+
+    # print(" ".join(str(user) for user in users))
+    # print()
