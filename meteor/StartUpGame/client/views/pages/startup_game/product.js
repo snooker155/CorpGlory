5d1c403259488554e576eda64c1220393c8ee171
Anton <anton.zubarev@crm-uni.de>
2015-10-17 16:28:02 +0300

    Tech fi

--- meteor/StartUpGame/client/views/pages/startup_game/product.js
+++ meteor/StartUpGame/client/views/pages/startup_game/product.js
@@ -1,46 +1,42 @@
-var max_level_number = new ReactiveVar(5);
-var level_number = new ReactiveVar(1);
+var max_tech_level_number = new ReactiveVar(5);
+var tech_level_number = new ReactiveVar(1);
 
 Template.product.helpers({
-	level_number: function(){
-		return level_number.get();
+	tech_level_number: function(){
+		return tech_level_number.get();
 	},
 
-	max_level_number: function(){
-		return max_level_number.get();
+	max_tech_level_number: function(){
+		return max_tech_level_number.get();
 	},
 
 	tech_level_plus_enabled:function(){
-		return level_number.get()<max_level_number.get()?"":"disabled";
+		return tech_level_number.get()<max_tech_level_number.get()?"":"disabled";
 	},
 
 	tech_level_minus_enabled:function(){
-		return level_number.get()>0?"":"disabled";
+		return tech_level_number.get()>0?"":"disabled";
 	},
 });
 
 Template.product.events({
 	"click #tech_level_minus": function(event){
-		if (!this.disabled){
-			level_number.set(level_number.get() - 1);
+			tech_level_number.set(tech_level_number.get() - 1);
 			var obj = {
 				"feature": "tech_level",
 				"value": -1
 			};
 			send('company_change', obj);
 			console.log(obj);
-		}
 	},
 
 	"click #tech_level_plus": function(event){
-		if (!this.disabled){
-			level_number.set(level_number.get() + 1);
+			tech_level_number.set(tech_level_number.get() + 1);
 			var obj = {
 				"feature": "tech_level",
 				"value": 1
 			};
 			send('company_change', obj);
 			console.log(obj);
-		}
 	}
 });
