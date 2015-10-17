5472d50441064a1e795b4d66544ee03712401dde
Константин <bakharev.k@gmail.com>
2015-10-17 19:18:20 +0300

    threshold added

--- server/Game/Elements/UserElement.py
+++ server/Game/Elements/UserElement.py
@@ -1,6 +1,6 @@
 __author__ = 'eduar'
 
-from Game.Managers.UserManager import update_friends
+from Game.Managers.UserManager import *
 from Game.Elements.GameElement import GameElement
 
 
@@ -10,3 +10,10 @@ class UserElement(GameElement):
 
     def update(self):
         update_friends(self.user_model)
+        update_relation_coefficient(self.user_model)
+
+        update_news(self.user_model)
+        update_news_coefficient(self.user_model)
+
+        update_inary(self.user_model)
+        update_products(self.user_model)
--- server/Game/Models/UserModel.py
+++ server/Game/Models/UserModel.py
@@ -7,18 +7,20 @@ Follower = namedtuple('Follower', ['user', 'weight'])
 class User:
     def __init__(self, id, name):
         self.id = id
-        self.ceo = False
-        self.product = None
-        self.name = name
-        self.liberal = Liberal.conservative
         self.friends = []
+
+        self.product = None
+        self.threshold = 0.0
         self.selfish = 0
         self.loyalty = defaultdict(float)
+
+        self.ceo = False
+        self.name = name
 
     def __str__(self):
         return str(self.loyalty)
 
 
-class Liberal(Enum):
-    conservative = 0
-    liberal = 1
+# class Liberal(Enum):
+#     conservative = 0
+#     liberal = 1
