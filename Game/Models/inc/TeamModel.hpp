eaff6f0087acea4a7d1dd27de745aa0c5f589831
EduardovEduard <eduardov.eduard.s@gmail.com>
2015-11-14 22:50:50 +0300

    Added missing files part 2

---
+++ Game/Models/inc/TeamModel.hpp
@@ -0,0 +1,15 @@
+//
+// Created by ees on 11/9/15.
+//
+
+#ifndef CORPGLORYGAME_TEAMMODEL_HPP
+#define CORPGLORYGAME_TEAMMODEL_HPP
+
+#include "Model.hpp"
+
+struct TeamModel: public Model
+{
+};
+
+
+#endif //CORPGLORYGAME_TEAMMODEL_HPP
