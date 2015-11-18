ede7464bde31f3eb87adfb5e6af6f27a674873fd
Alexey Velikiy <gmpota@gmail.com>
2015-11-18 14:32:32 +0300

    rm python crap

--- server/Game/Elements/GameElement.py
+++
@@ -1,7 +0,0 @@
-from abc import ABCMeta, abstractmethod
-
-
-class GameElement(metaclass=ABCMeta):
-    @abstractmethod
-    def update(self):
-        pass
--- server/Game/Models/WorldModel.py
+++
@@ -1,9 +0,0 @@
-class WorldModel:
-    def __init__(self, users, companies, all_news):
-        self.companies = companies
-        self.users = users
-        self.news = None
-
-        self.all_news = all_news
-        self.game_over = False
-
