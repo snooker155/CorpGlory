c3fe5f055e83d06e92e62dee57d5291ac097370a
Alexey Velikiy <gmpota@gmail.com>
2015-11-18 17:44:30 +0300

    client world state output via socket.io

---
+++ server/app.js
@@ -0,0 +1,36 @@
+// REQUIRES
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var gameNativeModule = require("../Game/NodeWrapper/build/Release/GameWrapper.node");
+
+// GAME CONFIG
+var players = [{name: "Eduard"}, {name: "Anton"}, {name: "Alexey"}];
+var game = gameNativeModule.NewGame(players);
+
+// -----------------------
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(4000, function() {
+  console.log('listening on *:3000');
+});
+
+// -----------------------
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
+  socket.on('userAction', function(msg) {
+    console.log('message: ' + msg);
+  });
+});
+
+setInterval(function() {
+  io.emit('nextGameState', game.gameState());
+}, 1000);
+
+
