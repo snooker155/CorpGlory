9a24b0b942812ed1530712b6cc47563e4d352c5e
Alexey Velikiy <gmpota@gmail.com>
2015-11-22 19:26:54 +0300

    dynamic market share imp

--- gameClient/client/views/gameScreen.js
+++ gameClient/client/views/gameScreen.js
@@ -351,7 +351,13 @@ Template.gameScreen.onRendered(function () {
   // Communications
   Communication.onNextState = function(stage) {
     var money = stage.world.ptr_wrapper.data.model.ptr_wrapper.data.companies[0].ptr_wrapper.data.money;
-    $("#balanceHolderValue").text(money + "$");
+    var companies = stage.world.ptr_wrapper.data.model.ptr_wrapper.data.companies;
+    var shares = {};
+    for(var i = 0; i < companies.length; i++) {
+      shares[companies[i].ptr_wrapper.data.name] = companies[i].ptr_wrapper.data.market_share;
+    }
+    MarketShare.updateBalance(money);
+    MarketShare.updateCompanyShares(shares);
   };
   
   Communication.open();
