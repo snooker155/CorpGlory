6e0f284637fe57ec7a82cf2b952735fc0e99b91d
Alexey Velikiy <gmpota@gmail.com>
2015-12-11 18:26:52 +0300

    basic eng version

--- server/app.js
+++ server/app.js
@@ -21,17 +21,27 @@ if(process.argv.length > 2) {
 
 var app = express();
 
+// views
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// middleware
 app.use("/public", express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.json());                           // to process 
+app.use(bodyParser.urlencoded({ extended: true }));   // request params
 
 // ROUTES
 
 app.get('/', function(req, res) {
-  res.render('index');
+  var host = req.get('host');
+  var language = 'en';
+  if(host.indexOf(".ru") != -1) {
+    language = 'ru';
+  }
+  res.render(
+    'index',
+    { language: language }
+  );
 });
 
 app.get('/subscribtions', function(req, res) {
