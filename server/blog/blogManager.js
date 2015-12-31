83004a7dfc696e29a1c68f64ae7530f7f0717726
Alexey Velikiy <gmpota@gmail.com>
2015-12-31 13:44:13 +0300

    mv from posts/data.xml to posts/data.json

--- server/blog/blogManager.js
+++ server/blog/blogManager.js
@@ -1,11 +1,13 @@
 const _ = require('underscore');
-const libxmljs = require("libxmljs");
 const fs = require('fs');
 const path = require('path');
+const jsonData = require('./posts/data.json');
 
 var posts = undefined;
 
 var BlogManager = {};
+
+const PREVIEW_DELIMETER = '<preview/>';
 
 // TODO: make async
 BlogManager.getPostText = function(id) {
@@ -16,28 +18,17 @@ BlogManager.getPostText = function(id) {
 BlogManager.loadAllPosts = function() {
   function getPreview(id) {
     var text = BlogManager.getPostText(id);
-    var cutIndex = text.indexOf('<preview/>');
+    var cutIndex = text.indexOf(PREVIEW_DELIMETER);
     if(cutIndex !== -1) {
       text = text.substring(0, cutIndex);
     }
     return text;
   }
-  var p = path.join(__dirname, 'posts', 'data.xml');
-  var xmlText = fs.readFileSync(p, 'utf8');
-  var xmlDoc = libxmljs.parseXml(xmlText);
-  var xmlPosts = _.filter(
-    xmlDoc.root().childNodes(),
-    n => n.name() === 'post'
-  );
-  var posts = _.map(xmlPosts, xp => {
-    var id = xp.attr('id').value();
-    var title = xp.attr('title').value();
-    var previewHtml = getPreview(id);
-    return {
-      id: id, 
-      previewHtml: previewHtml,
-      title: title
-    };
+
+  var posts = _.map(jsonData.posts, jp => {
+    var res = _.clone(jp);
+    res.previewHtml = getPreview(jp.id);
+    return res;
   });
 
   var res = _.object(
@@ -54,7 +45,7 @@ BlogManager.getPost = function(postId) {
     return undefined;
   }
   var text = BlogManager.getPostText(postId);
-  text.replace('<preview/>', '');
+  text.replace(PREVIEW_DELIMETER, '');
   var res = _.clone(BlogManager.posts[postId]);
   res.html = text;
   return res;
